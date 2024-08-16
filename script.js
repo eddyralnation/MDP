3075ca5c2234778e063a0b94e6b6e7d73df09f2e
Harishwa <138890307+eddyralnation@users.noreply.github.com>
2024-08-16 20:20:53 +0530

    Initial commit by envi

---
+++ script.js
@@ -0,0 +1,55 @@
+gsap.from("#p1",{
+      scrollTrigger : {
+        scrub: true
+      },
+      y:250,
+      scale:2.5,
+    }) 
+
+gsap.from("#p2",{
+      scrollTrigger : {
+        scrub: true
+      },
+      X:260,
+    }) 
+
+gsap.from("#p3",{
+      scrollTrigger : {
+        scrub: true
+      },
+      Y:-150,
+      scale:1.5,
+
+    })
+
+gsap.from("#p4",{
+      scrollTrigger : {
+        scrub: true
+      },
+      Y:-550,
+      scale:1.5,
+    })
+
+gsap.from("#p5",{
+      scrollTrigger : {
+        scrub: true
+      },
+      Y:-750,
+      scale:1.5,
+    })
+
+
+gsap.from("#p8",{
+      scrollTrigger : {
+        scrub: true
+      },
+      y:450,
+    })
+
+gsap.from("#p9",{
+      scrollTrigger : {
+        scrub: true
+      },
+      opacity:100,
+    }) 
+
